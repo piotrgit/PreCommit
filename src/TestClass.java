09dcb97f2603257a177e6fc2e47c814a8dd8b58b
piotrgit <pmaruszak@krusovice.(none)>
2011-01-24 09:46:43 +0100

    Initial

---
+++ src/TestClass.java
@@ -0,0 +1,15 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: pmaruszak
+ * Date: 1/24/11
+ * Time: 9:45 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestClass {
+    private final int value = 0;
+
+    public TestClass() {
+    }
+
+
+}
