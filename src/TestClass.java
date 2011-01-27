335175c7481a546c6f4d73a216994ab027498bf0
piotrgit <pmaruszak@krusovice.(none)>
2011-01-27 10:44:11 +0100

    Initial

--- src/TestClass.java
+++ src/TestClass.java
@@ -6,10 +6,18 @@
  * To change this template use File | Settings | File Templates.
  */
 public class TestClass {
-    private final int value = 0;
+    private  int value = 0;
+    private int tmp = 123;
+    private int b;
 
     public TestClass() {
     }
 
+    public int getValue() {
+        return value;
+    }
 
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
